f29c71e3e6f8f8802adec983e65e54cd7790b93b
Sean Landis <sean.a.landis@gmail.com>
2023-04-05 19:10:46 -0700

    random masses

--- Assets/BlockController.cs
+++ Assets/BlockController.cs
@@ -4,17 +4,29 @@ using UnityEngine;
 
 public class BlockController : MonoBehaviour
 {
+    public float upwardForce;
+    public float downwardForce;
     public float lateralForce;
     public float torqueForce;
+    public int[] masses = {10, 50 ,300};
+    public Color[] colors = {Color.white, Color.gray, Color.black};
     public bool disconnected = false;
+    private SpriteRenderer mySpriteRenderer;
     private Rigidbody2D myRigidbody;
     private BoxCollider2D myCollider;
+    private bool downwardUsed = false;
+    private int index;
 
     // Start is called before the first frame update
     void Start()
     {
+        mySpriteRenderer = gameObject.GetComponent<SpriteRenderer>();
         myRigidbody = gameObject.GetComponent<Rigidbody2D>();
         myCollider = gameObject.GetComponent<BoxCollider2D>();
+
+        index = Random.Range(0, 3);
+        myRigidbody.mass = masses[index];
+        mySpriteRenderer.color = colors[index];
     }
 
     // Update is called once per frame
@@ -26,23 +38,28 @@ public class BlockController : MonoBehaviour
 
         if (!disconnected) {
             if (Input.GetKeyDown(KeyCode.W)) {
-                myRigidbody.AddForce(Vector2.up * 100);
+                myRigidbody.AddForce(Vector2.up * upwardForce * masses[index]);
             }
 
             if (Input.GetKeyDown(KeyCode.A)) {
-                myRigidbody.AddForce(Vector2.left * lateralForce);
+                myRigidbody.AddForce(Vector2.left * lateralForce * masses[index]);
             }
 
             if (Input.GetKeyDown(KeyCode.D)) {
-                myRigidbody.AddForce(Vector2.right * lateralForce);
+                myRigidbody.AddForce(Vector2.right * lateralForce * masses[index]);
+            }
+
+            if (!downwardUsed && Input.GetKeyDown(KeyCode.S)) {
+                myRigidbody.AddForce(Vector2.down * downwardForce * masses[index]);
+                downwardUsed = true;
             }
 
             if (Input.GetKeyDown(KeyCode.E)) {
-                myRigidbody.AddTorque(torqueForce);
+                myRigidbody.AddTorque(torqueForce * masses[index]);
             }
 
             if (Input.GetKeyDown(KeyCode.Q)) {
-                myRigidbody.AddTorque(-1 * torqueForce);
+                myRigidbody.AddTorque(-1 * torqueForce * masses[index]);
             }
         }
     }
